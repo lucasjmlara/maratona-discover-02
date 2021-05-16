5ae39a74d8eaf8c9e0e827717041a5c8fd3ffbac
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2021-05-16 19:09:34 -0300

    Quase fim da aula 1

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const server = express();
+const routes = require("./routes");
+
+//habilitar arquivos estaticos
+server.use(express.static("public"));
+
+//routes
+server.use(routes);
+
+server.listen(3000, () => console.log('rodando'));
