601e64a9b032cac542fc3a8b4e108d1c05cefed2
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2021-05-26 21:06:14 -0300

    Aula 3 - dia 3

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -13,12 +13,16 @@ module.exports = {
             total: jobs.length,
         }
     
+        let jobTotalHours = 0;
+
         const updatedJobs = jobs.map((job) => {
           const remaining = JobUtils.remainingDays(job);
           const status = remaining <= 0 ? "done" : "progress";
 
           statusCount[status] += 1;
-    
+
+          jobTotalHours = (status == "progress") ? jobTotalHours + Number(job['daily-hours']) : jobTotalHours;
+          
           return {
             ...job,
             remaining,
@@ -26,7 +30,9 @@ module.exports = {
             budget: JobUtils.calculateBudget(job, profile["value-hour"]),
           };
         });
+
+        const freeHours = profile['hours-per-day'] - jobTotalHours;
     
-        return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount });
+        return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours});
       },
 }
